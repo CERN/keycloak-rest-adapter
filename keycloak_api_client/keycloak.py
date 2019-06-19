0f6366488f46fb7417be9f06cef1b70146de2a6e
Cristian Schuszter <cristian.schuszter@cern.ch>
2019-06-19 14:56:31 +0000

    Default consent required = False

--- keycloak_api_client/keycloak.py
+++ keycloak_api_client/keycloak.py
@@ -662,6 +662,9 @@ class KeycloakAPIClient(object):
         # by default, create confidential client
         if "publicClient" not in kwargs:
             kwargs["publicClient"] = False
+        # by default don't require consent to login (it's hardcoded to true in keycloak)
+        if "consentRequired" not in kwargs:
+            kwargs["consentRequired"] = False
         if "protocol" not in kwargs or kwargs["protocol"] != "openid-connect":
             # on API level we accept 'openid-connect' & 'openid'. Keycloak only accepts 'openid-connect'
             kwargs["protocol"] = "openid-connect"
